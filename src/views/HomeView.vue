e1053cc501635c5135f04970ddcc732cee973faf
opew12 <opew.st12@gmail.com>
2022-10-22 16:31:20 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="My logo" src="../assets/logo.png" height="100" width="100" />
+    <HelloWorld msg="" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
